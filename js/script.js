522c2627287cd871f55593757b3d7da0ed28b1e1
Sara Isabel del Sastre <chinchimilla@gmail.com>
2025-11-10 16:25:04 +0100

    Reto1-Sprint4

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
 import peliculas from './peliculas.js'
 
+const accion = peliculas.filter(element => element.genre_ids.includes(28));
+const thriller = peliculas.filter(element => element.genre_ids.includes(53));
+const aventura = peliculas.filter(element => element.genre_ids.includes(12));
+
+const box1 = document.querySelector(".genero");
+const box2 = document.querySelectorAll(".genero")[1];
+const box3 = document.querySelectorAll(".genero")[2];
+
+
+function pintarPeliculas(pelis, container){
+    container.innerHTML = pelis.map(pelicula => `
+        <div class="card">
+            <img src="https://image.tmdb.org/t/p/w200${pelicula.poster_path}" alt="${pelicula.title}">
+            <p>${pelicula.title}</p>
+        </div>
+    `).join('');
+}
+
+pintarPeliculas(accion, box1);
+pintarPeliculas(thriller, box2);
+pintarPeliculas(aventura, box3);
